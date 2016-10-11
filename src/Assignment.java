a6de2a2b984dd34b46212532071f27d511cad7ae
Mitchell Harris <mitchell.harris1995@gmail.com>
2016-10-12 08:32:16 +1100

    Added a main method and the input.txt file

---
+++ src/Assignment.java
@@ -0,0 +1,16 @@
+/*
+ * Students: Mitchell Harris (3184553), Joshua Wilson (3166052)
+ * Course: COMP2230 Algorithmics
+ * Assessment: Assignment
+ *
+ * Class name: Assignment
+ * Description: TODO
+ */
+
+public class Assignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
