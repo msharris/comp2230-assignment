5058953fc9aa41f09d9bee511fabba8e712b8266
Mitchell Harris <mitchell.harris1995@gmail.com>
2016-10-22 17:02:20 +1100

    Moved assignment class logic into a non-static context

--- src/Assignment.java
+++ src/Assignment.java
@@ -14,11 +14,17 @@ import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 public class Assignment {
-    private static ArrayList<HotSpot> hotSpots = new ArrayList<>();	//HotSpots stored array list
-    private static ArrayList<Edge> setOfEdges = new ArrayList<>();  //The set of all edges in the graph (excluding edges that loop on one vertex)
-    private static DecimalFormat formatter = new DecimalFormat("0.00");    	//2 decimal place format for doubles
+
+    private ArrayList<HotSpot> hotSpots = new ArrayList<>();        //HotSpots stored array list
+    private ArrayList<Edge> setOfEdges = new ArrayList<>();         //The set of all edges in the graph (excluding edges that loop on one vertex)
+    private DecimalFormat formatter = new DecimalFormat("0.00");    //2 decimal place format for doubles
 
     public static void main(String[] args) {
+        Assignment assignment = new Assignment();
+        assignment.run();
+    }
+
+    private void run() {
         readIn();			//Read input file
         initialGreeting();	//Greeting and initial messages
         weightedGraph();	//Print the weighted graph, this also collects the set of all edges
@@ -42,7 +48,7 @@ public class Assignment {
         System.out.println("Thank you for using Kruskal’s Clustering. Bye.");
     }
 
-    public static void readIn() {
+    private void readIn() {
         File file = new File("input.txt");
         try {
             Scanner in = new Scanner(file);
@@ -58,23 +64,23 @@ public class Assignment {
             System.out.println("File not found");
         }
     }
-    public static void initialGreeting() {
+    private void initialGreeting() {
         System.out.println("Hello and welcome to Kruskal’s clustering!\n");
         System.out.println("There are " + hotSpots.size() + " hotspots.\n");
         System.out.println("The weighted graph of hotspots: \n");
     }
-    public static void menu() {
+    private void menu() {
         System.out.println("\nHow many emergency stations would you like?");
         System.out.println("(Enter a number between 1 and 5 to place the emergency stations.\n Enter -1 to automatically select the number of emergency stations.\n Enter 0 to exit.)\n\n");
     }
 
-    public static double distanceBetween(HotSpot s1, HotSpot s2) {
+    private double distanceBetween(HotSpot s1, HotSpot s2) {
         double distance = sqrt(pow((s2.getX()-s1.getX()),2) + pow((s2.getY()-s1.getY()),2));	//Euclidean Distance formula
         return Double.parseDouble(formatter.format(distance));									//Round to two d.p. and covert back to double
         //TODO: Note, not rounded, just chopping off everything after 2 d.p. - look into rounding!
     }
 
-    public static void weightedGraph() {
+    private void weightedGraph() {
         for(int i = 0; i < hotSpots.size(); i++) {
             String currentLine = "";
             for(int j = 0; j < hotSpots.size(); j++) {
@@ -88,7 +94,7 @@ public class Assignment {
         }
     }
 
-    public static boolean containsReverse(Edge original) {
+    private boolean containsReverse(Edge original) {
         Edge swapped = new Edge(original.getV2(), original.getV1(), original.getWeight());  //Create a new edge, with the vertices swapped
         if(setOfEdges.contains(swapped)) {          //If the swapped vertices create an edge thats already in the set of edges
             return true;                            //True - don't add to list again
@@ -99,7 +105,7 @@ public class Assignment {
 
 
 
-    public static void findCentroid(ArrayList<HotSpot> cluster) {
+    private void findCentroid(ArrayList<HotSpot> cluster) {
         double xTotal = 0;
         double yTotal = 0;
         for(int i = 0; i < cluster.size(); i++) {
@@ -111,13 +117,13 @@ public class Assignment {
         //New station(centroidX, centroidY)?
     }
 
-    public static void autoSelect() {
+    private void autoSelect() {
         //TODO
     }
 
-    public static void userDefined(int numberOfStations) {
+    private void userDefined(int numberOfStations) {
         //TODO
-        ArrayList<ArrayList<Edge>> disjointSets;    ///maybe??
+        ArrayList<ArrayList<Edge>> disjointSets = null;    ///maybe??
         ArrayList<Edge> addedEdges = new ArrayList<>();
         ArrayList<Edge> unaddedEdges = setOfEdges;
         while(disjointSets.size() < numberOfStations) {
@@ -132,12 +138,14 @@ public class Assignment {
         }
     }
 
-    public static Edge searchShortestEdge(ArrayList<Edge> list) {
+    private Edge searchShortestEdge(ArrayList<Edge> list) {
         //TODO: This will just search through the unadded edges and return the next shortest one
+        return null;
     }
 
-    public static boolean createsCycle(Edge test, ArrayList<Edge> addedEdges) {
+    private boolean createsCycle(Edge test, ArrayList<Edge> addedEdges) {
         //TODO: This will take the added edges and test to see if the edge we are looking to add will create a cycle
+        return false;
     }
 
 }
